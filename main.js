61c84e728a1447679ef180dfaf081f0d48a2753b
neerajsingh012625-rgb <neerajsingh012625@gmail.com>
2026-01-21 21:21:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-gsap.timeline()
-.to(".stem",{height:220,duration:2})
-.to(".rose",{scale:1,duration:1.5})
-.to(".petals span",{opacity:1,y:200,x:()=>gsap.utils.random(-100,100),stagger:0.4,duration:4},"-=1")
-.to(".text",{opacity:1,y:-20,duration:2});
+const audio = document.getElementById("bgm");
+document.body.addEventListener("click", () => audio.play(), { once:true });
+
+const tl = gsap.timeline();
+
+tl.to(".t1", { opacity:1, duration:2 })
+  .to(".rose", { scale:1, duration:2, ease:"elastic.out(1,0.4)" })
+  .to(".t1", { opacity:0, duration:1, delay:2 })
+
+  .to(".t2", { opacity:1, duration:2 })
+  .to(".rose", { scale:0, duration:1 })
+  .to(".t2", { opacity:0, duration:1, delay:1 })
+
+  .to(".sunflower", { scale:1, duration:2, ease:"elastic.out(1,0.4)" })
+  .to(".t3", { opacity:1, duration:2 })
+
+  .to(".photos img:nth-child(1)", { opacity:1, duration:2 }, "+=1")
+  .to(".photos img:nth-child(2)", { opacity:1, duration:2 })
+  .to(".photos img:nth-child(3)", { opacity:1, duration:2 })
+
+  .to(".final", { opacity:1, duration:3, delay:2 });
